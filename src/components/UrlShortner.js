c75ab87bea8879ccd36688f2dc3afa427f5740c8
Rahul Ghimire <ghimirerahul554@gmail.com>
2024-04-23 16:32:38 +0545

    add feature in enter url

--- src/components/UrlShortner.js
+++ src/components/UrlShortner.js
@@ -4,20 +4,16 @@ import axios from "axios";
 const UrlShortner = () => {
   const [longUrl, setLongUrl] = React.useState("");
   const [customUrl, setCustomUrl] = React.useState("");
-  const [data,setData]=React.useState(null);
+  const [data, setData] = React.useState(null);
   const apiKey = "aOMZquUWHTONbKvmbeJdwrz58JC5FVKMhoseuMSDpvFkPlQe9v0ozFalrvhN";
-  const  url="https://api.tinyurl.com/create"
+  const url = "https://api.tinyurl.com/create";
 
-//   var headers = {
-//     apikey: apiKey,
-//     // workspace: "YOUR_WORKSPACE_ID"
-//   }
+  //   var headers = {
+  //     apikey: apiKey,
+  //     // workspace: "YOUR_WORKSPACE_ID"
+  //   }
 
-// const url = 'https://tinyurl.com/app/dev/authenticate'
-
-
-
-
+  // const url = 'https://tinyurl.com/app/dev/authenticate'
 
   const handleLongUrl = (e) => {
     setLongUrl(e.target.value);
@@ -29,21 +25,26 @@ const UrlShortner = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    axios.post(url, { url: customUrl }, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${apiKey}`
+    axios
+      .post(
+        url,
+        { url: longUrl },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${apiKey}`,
+          },
         }
-    })
-    .then(response => {
-        console.log(response.data);
-    })
-    .catch(error => {
+      )
+      .then((response) => {
+        const tinyUrl = response.data?.data?.tiny_url;
+        if (tinyUrl) {
+          setData(tinyUrl);
+        }
+      })
+      .catch((error) => {
         console.error(error);
-    });
-    
-
+      });
   };
 
   return (
@@ -71,7 +72,7 @@ const UrlShortner = () => {
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
         </div>
-
+        {data && <div>{data}</div>}
         <button
           type="submit"
           class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
